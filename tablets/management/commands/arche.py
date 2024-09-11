8a8f31dbbf98898a48b2eb248bc88897b5ca8e74
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-11 14:08:50 +0200

    add arche-md script; wip arche-md generation

--- tablets/management/commands/arche.py
+++ tablets/management/commands/arche.py
@@ -1,8 +1,9 @@
 import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from rdflib import Namespace, Graph, URIRef, Literal, RDF
 
-from rdflib import Namespace, Graph
+from tablets.models import Tablet
 
 
 class Command(BaseCommand):
@@ -10,6 +11,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         SEED_FILE_DIR = settings.SEED_FILE_DIR
+        ACDH = Namespace("https://vocabs.acdh.oeaw.ac.at/schema#")
+        LABASI = Namespace("https://id.acdh.oeaw.ac.at/labasi/")
         ARCHE_ROOT = settings.ARCHE_ROOT
         ARCHE_MD_FILE = os.path.join(ARCHE_ROOT, "arche.ttl")
 
@@ -18,10 +21,21 @@ class Command(BaseCommand):
             os.path.join(SEED_FILE_DIR, "repo_objects_constants.ttl")
         )
 
-        ACDH = Namespace("https://vocabs.acdh.oeaw.ac.at/schema#")
+        for x in Tablet.objects.all():
+            uri = URIRef(LABASI[f"tablet_{x.id:03}.xml"])
+            g.add((uri, RDF.type, ACDH["Resource"]))
+            g.add((uri, ACDH["hasTitle"], Literal(x.title, lang="und")))
+            g.add(
+                (
+                    uri,
+                    ACDH["isPartOf"],
+                    URIRef("https://id.acdh.oeaw.ac.at/labasi/tablets"),
+                )
+            )
+
+        print("adding repo objects constants now")
         COLS = [ACDH["TopCollection"], ACDH["Collection"], ACDH["Resource"]]
         COL_URIS = set()
-
         for x in COLS:
             for s in g.subjects(None, x):
                 COL_URIS.add(s)
